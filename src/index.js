1e2fcbc45f6da03f525ca1636710ff46fef025a2
ajc133 <16407457+ajc133@users.noreply.github.com>
2021-02-15 22:12:42 -0800

    Add proper clicking and submitting buttons

--- src/index.js
+++ src/index.js
@@ -11,21 +11,35 @@ class Board extends React.Component {
   renderTile(i) {
     return (
       <Tile
-        letter={this.props.tiles[i]}
-        onClick={() => this.props.onClick(i)}
+        letter={this.props.outerLetters[i]}
+        onClick={() => this.props.onClickOuter(i)}
       />
     );
+  }
+
+  renderCenter() {
+    return (
+          <Tile
+            letter={this.props.centerLetter}
+            onClick={() => this.props.onClickInner()}
+          />
+    )
   }
 
   render() {
     return (
       <div>
-        {this.renderTile(0)}
-        {this.renderTile(1)}
-        {this.renderTile(2)}
-        {this.renderTile(3)}
-        {this.renderTile(4)}
-        {this.renderTile(5)}
+        <div className="inner">
+          {this.renderCenter()}
+        </div>
+        <div className="outer">
+          {this.renderTile(0)}
+          {this.renderTile(1)}
+          {this.renderTile(2)}
+          {this.renderTile(3)}
+          {this.renderTile(4)}
+          {this.renderTile(5)}
+        </div>
       </div>
     )
   }
@@ -35,28 +49,54 @@ class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      tiles: ['a','b','c','d','e','f'],
-      stepNumber: 0,
-      history: []
+      word: ""
     }
+    this.outerLetters = ['a','b','c','d','e','f'];
+    this.centerLetter = 's';
   };
 
-  handleClick(i) {
+  handleClickOuter(i) {
+    let letter = this.outerLetters[i];
+    this.handleClick(letter);
+  }
+
+  handleClickInner() {
+    let letter = this.centerLetter.slice();
+    this.handleClick(letter);
+  }
+
+  handleClick(letter) {
     this.setState({
-      history: this.state.history.concat([i]),
-      stepNumber: this.state.stepNumber + 1
+      word: this.state.word + letter,
     })
+  }
+  
+  // Has to be an arrow function to properly bind `this`
+  submitWord = () => {
+    alert(this.state.word);
+  }
+
+  backspace = () => {
+    this.setState({
+      word: this.state.word.slice(0, -1)
+    });
   }
 
   render() {
-    const tiles = this.state.tiles.slice();
+    const outerLetters = this.outerLetters.slice();
     return (
       <div>
-      <div className="info">{this.state.stepNumber} - {this.state.history} </div>
-      <Board 
-        tiles={tiles}
-        onClick={(i) => this.handleClick(i)}
-      />
+        <div className="info">
+          {this.state.word.length} - {this.state.word}
+        </div>
+        <Board
+          outerLetters={outerLetters}
+          centerLetter={this.centerLetter}
+          onClickOuter={(i) => this.handleClickOuter(i)}
+          onClickInner={() => this.handleClickInner()}
+        />
+        <button id="deleteButton" onClick={this.backspace}> Delete </button>
+        <button id="submitButton" onClick={this.submitWord} > Submit </button>
       </div>
     )
   }
